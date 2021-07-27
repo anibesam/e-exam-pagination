e3ccb91514b34ea539b1c581feeb7f07562943b2
Anibe Sam <13267108+sambezalel@users.noreply.github.com>
2021-07-27 16:17:37 +0100

    msyql database connection

--- connection.php
+++ connection.php
@@ -1,9 +1,9 @@
 <?php
 $server='localhost';
-$uname='root';
-$password='2344454';
-$dbase='my_cbt';
-$con = new mysqli($server,$uname,$password,$dbase);
+$username='root';
+$password='234';
+$database='my_cbt';
+$connection = new mysqli($server,$uname,$password,$dbase);
 //Eror Handler
 if($con->connect_error){
 	printf("Connection Failed: %s\n",$con->connect_error);
